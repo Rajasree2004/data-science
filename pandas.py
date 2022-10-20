ab4c8478020a5afb16d841330470ebc6e55d71e2
Rajasree2004 <rajasree.selva@gmail.com>
2022-10-21 04:25:42 +0530

    multiIndex and Indec Hierarchy

--- pandas.py
+++ pandas.py
@@ -41,3 +41,16 @@ df.reset_index()
 newind = 'CA NY WY OR CO'.split()
 df['States'] = newind
 df.set_index("States") 
+
+#DataFrame-III
+outside = ['G1','G1','G1','G2','G2','G2']
+inside = [1,2,3,1,2,3]
+hier_index = list(zip(outside,inside))
+hier_index = pd.MultiIndex.from_tuples(hier_index)
+df = pd.DataFrame(randn(6,2),hier_index,['A','B'])
+df.loc['G1']
+df.loc['G1'].loc[1]
+df.index.names = ['Groups', 'Number']
+df.loc['G2'].loc[2]['B']
+df.xs('G1')
+df.xs(1, level='Number')
