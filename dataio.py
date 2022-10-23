87a8c032ecfffc7e56c67a7345833deb38e25d8a
Rajasree2004 <rajasree.selva@gmail.com>
2022-10-23 13:57:36 +0530

    read from excel

--- dataio.py
+++ dataio.py
@@ -2,6 +2,12 @@ import numpy as np
 import pandas as pd
 df = pd.read_csv('example')
 print(df)
-df.to_csv('my_output',index=False)
-dfo = pd.read_csv('my_output')
-print(dfo)
+# df.to_csv('my_output',index=False)
+# dfo = pd.read_csv('my_output')
+# print(dfo)
+#Excel
+dfe = pd.read_excel('Excel_Sample.xlsx')
+print(dfe)
+dfe.to_excel('Excel_Sample2.xlsx',index=False)
+# dfeo = pd.read_excel("Excel_Sample2.xlsx")
+# print(dfeo)
