9cc3f5bff255af0361d3013aa3e987cb553f51a7
Rajasree2004 <rajasree.selva@gmail.com>
2022-10-25 09:06:49 +0530

    read from sql

--- dataio.py
+++ dataio.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from sqlalchemy import create_engine
 df = pd.read_csv('example')
 print(df)
 # df.to_csv('my_output',index=False)
@@ -13,7 +14,11 @@ dfe.to_excel('Excel_Sample2.xlsx',index=False)
 # print(dfeo)
 
 #HTML
-data = df = pd.read_html('http://www.fdic.gov/bank/individual/failed/banklist.html')
-print(data[0].head())
+# data = df = pd.read_html('http://www.fdic.gov/bank/individual/failed/banklist.html')
+# print(data[0].head())
 
 #SQL
+engine = create_engine('sqlite:///:memory:')
+df.to_sql('my_table', engine)
+dfs = pd.read_sql('my_table',con=engine)
+print(dfs)
