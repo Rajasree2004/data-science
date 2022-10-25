aa42cf3f614af4ce968ff6200142032d61d0b045
Rajasree2004 <rajasree.selva@gmail.com>
2022-10-25 10:44:06 +0530

    multiplots

--- mp.py
+++ mp.py
@@ -5,10 +5,17 @@ y = x**2
  #Functional method
 
 plt.plot(x,y,'r-')
-plt.show()
-
 plt.xlabel('X Label')
 plt.ylabel('Y Label')
 plt.title('Title')
+plt.show()
 
 
+#multiplots
+##plot1
+plt.subplot(1,2,1)
+plt.plot(x,y,'r')
+##plot2
+plt.subplot(1,2,2)
+plt.plot(y,x,'b')
+plt.show()
